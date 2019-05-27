c0370d28449213cfcee134b5613ce6b6ad5c4b6e
Harish Hardy <mj.harishardy@gmail.com>
2019-05-27 17:13:04 +0530

    initial commit

---
+++ sample1.py
@@ -0,0 +1,50 @@
+'''
+Created on 27 May 2019
+
+@author: harishhardy
+'''
+#print("Hello World")
+a = 26
+b = 654
+c = 8767
+d = 762
+ 
+name = "harishardy"
+ 
+# print((a+b)+(c+d))
+
+# if c > 200:
+#     print(c)
+#     if c % 2 == 0:
+#         print(c/2)
+#     else:
+#         print("Not even")
+# else:
+#     print("Less than 200")       
+
+
+# if a > 30:
+#     print("Its greater than 30")
+# elif a % 2 == 0:
+#     print("Its EVEN number")
+# else:
+#     print("Its ODD number")
+
+# for letter in name:
+#     print(letter),
+    
+#     
+# print(a),
+# print(b),
+# print(c)
+# 
+# print("harish", end = "dkjhvsdfv")
+#print('hardy')
+
+for x in range(101,1,-4):
+    print(x, end = ',')
+   
+    
+    
+
+
